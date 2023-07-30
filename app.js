8a2b3fb17ac1fa7271dc41622802f9bb63430003
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-30 13:44:36 -0400

    worked with BigInt Type for larger numbers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Numbers
 
-console.log(Number.MAX_SAFE_INTEGER) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1.
+console.log(Number.MAX_SAFE_INTEGER) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1 (9007199254740991).
 console.log(Math.pow(2, 53)-1) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1.
 console.log(Math.pow(2, 53)) // Number can be displayed, but not calculated.
 console.log(Math.pow(2, 53)+1) // Number does not increase from previous result.
@@ -13,3 +13,5 @@ console.log((5).toString(2))
 console.log((1/5).toString(2))
 console.log(.2.toFixed(20))
 console.log((.2 + .4).toFixed(2))
+
+console.log(9007199254740991n)
