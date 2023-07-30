c463d554854794bcaaeb5c1c6b921b6a04796335
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-30 12:13:12 -0400

    added styles

---
+++ app.js
@@ -0,0 +1,6 @@
+// Numbers
+
+console.log(Number.MAX_SAFE_INTEGER) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1
+console.log(Math.pow(2, 53)-1) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1
+console.log(Math.pow(2, 53)) // Number can be displayed, but not calculates
+console.log(Math.pow(2, 53)+1) // Number does not increase from previous result
