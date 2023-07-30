b39f5028937f8c67409b104f4fb53d53960da6f8
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-30 12:13:42 -0400

    added Numbers study code

--- app.js
+++ app.js
@@ -3,4 +3,4 @@
 console.log(Number.MAX_SAFE_INTEGER) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1
 console.log(Math.pow(2, 53)-1) // Biggest numer / no decimal - 9 quadrillion 2 to the power of 53 - 1
 console.log(Math.pow(2, 53)) // Number can be displayed, but not calculates
-console.log(Math.pow(2, 53)+1) // Number does not increase from previous result
+console.log(Math.pow(2, 53)+1) // Number does not increase from previous result.
