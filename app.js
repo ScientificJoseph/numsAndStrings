67f51f7fc2792db752d30f7e517336908550ce16
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-31 19:48:17 -0400

    study of template literals

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const randomIntBetween = ((min, max) => { //min: 5, max: 10
     return Math.floor(Math.random() * (max - min + 1) + min); //+1 added doe to random not reaching 1. If 10.999999 was the result, floor would round down to 10. 
 })
 
-console.log(randomIntBetween(3,10))
+// console.log(randomIntBetween(3,10))
 
 // Strings - Tagged Templates
 
 const name =  'Joseph';
 
-console.log(`My name is ${name}.`)
+// console.log(`My name is ${name}.`)
+
+const productDescription = ((strings, productName, productPrice) => { //first part is array of non dynamic items. Dynamic segments are passed in as additional arguments into function
+    console.log(strings)
+    console.log(productName)
+    console.log(productPrice)
+    let priceCategory = 'pretty cheap regarding its price';
+    if (productPrice > 20) {
+        priceCategory = 'fairly priced'
+    }
+    // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`
+    return {mane: productName, price:productPrice} // can return an object
+})
+
+const prodName = 'JavaScript Course'
+const prodPrice = 29.99
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`
+console.log(productOutput)
